23e8de2063c79f98ab3950c1156b7f0e216cea4b
chitam6337639 <chitam6337639@gmail.com>
2025-01-13 17:08:13 +0700

    setup cleanArchWithCQRS

---
+++ CleanArchWithCQRS.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchWithCQRS.Infrastructure
+{
+	public static class ConfigureServices
+	{
+		public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			return services;
+		}
+	}
+}
