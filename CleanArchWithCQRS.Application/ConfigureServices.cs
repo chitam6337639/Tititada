ff077fc878ba5fe62b4a1818e67d982e6fabd094
chitam6337639 <chitam6337639@gmail.com>
2025-01-15 13:49:32 +0700

    add Fluent Validation

--- CleanArchWithCQRS.Application/ConfigureServices.cs
+++ CleanArchWithCQRS.Application/ConfigureServices.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using CleanArchWithCQRS.Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArchWithCQRS.Application
@@ -13,9 +16,11 @@ namespace CleanArchWithCQRS.Application
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 		{
 			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+			services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 			services.AddMediatR(ctg =>
 			{
 				ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+				ctg.AddBehavior(typeof(IPipelineBehavior<,>) ,typeof(ValidationBehaviour<,>));
 			});
 			return services;
 		}
---
+++ CleanArchWithCQRS.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace CleanArchWithCQRS.Application.Products.Commands.CreateProduct
+{
+	public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+	{
+		public CreateProductCommandValidator() 
+		{
+			RuleFor(v => v.Name)
+				.NotEmpty().WithMessage("Name is required")
+				.MaximumLength(200).WithMessage("Name must not exceed 200 characters");
+
+			RuleFor(v => v.Description)
+				.NotEmpty().WithMessage("Description is required")
+				.MaximumLength(200).WithMessage("Description must not exceed 200 characters");
+
+			RuleFor(v => v.Price)
+				.NotEmpty().WithMessage("Price is required");
+		}
+	}
+}
