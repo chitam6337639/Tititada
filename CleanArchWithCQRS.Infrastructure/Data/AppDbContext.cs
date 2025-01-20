857cfeb00de1e72211218fe4c97f5bd96d3f3ef0
chitam6337639 <chitam6337639@gmail.com>
2025-01-20 17:12:15 +0700

    add identity

--- CleanArchWithCQRS.Infrastructure/Data/AppDbContext.cs
+++ CleanArchWithCQRS.Infrastructure/Data/AppDbContext.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CleanArchWithCQRS.Domain.Entities;
+using CleanArchWithCQRS.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchWithCQRS.Infrastructure.Data
 {
-	public class AppDbContext : DbContext
+	public class AppDbContext : IdentityDbContext<ApplicationUser>
 	{
 		public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
 		{
--- CleanArchWithCQRS.Infrastructure/Migrations/20250120083817_addIdentity.Designer.cs
+++ CleanArchWithCQRS.Infrastructure/Migrations/20250120083817_addIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CleanArchWithCQRS.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CleanArchWithCQRS.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250120083817_addIdentity")]
+    partial class addIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
