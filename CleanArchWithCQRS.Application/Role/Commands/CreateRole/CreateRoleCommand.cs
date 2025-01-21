f43018cb30d8ad359332c07c3a627c438a7999ea
chitam6337639 <chitam6337639@gmail.com>
2025-01-21 16:09:13 +0700

    add command Role&User

---
+++ CleanArchWithCQRS.Application/Role/Commands/CreateRole/CreateRoleCommand.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchWithCQRS.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchWithCQRS.Application.Role.Commands.CreateRole
+{
+	public class CreateRoleCommand : IRequest<int>
+	{
+		public string RoleName { get; set; }
+	}
+
+	public class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, int>
+	{
+		private readonly IIdentityService _identityService;
+
+		public CreateRoleCommandHandler(IIdentityService identityService)
+		{
+			_identityService = identityService;
+		}
+		public async Task<int> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+		{
+			var result = await _identityService.CreateRoleAsync(request.RoleName);
+			return result ? 1 : 0;
+		}
+	}
+}
---
+++ CleanArchWithCQRS.Application/Role/Queries/GetRoleQuery.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchWithCQRS.Application.Common.Interfaces;
+using CleanArchWithCQRS.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchWithCQRS.Application.Role.Queries
+{
+	public class GetRoleQuery : IRequest<IList<RoleResponseDTO>>
+	{
+
+	}
+
+	public class GetRoleQueryHandler : IRequestHandler<GetRoleQuery, IList<RoleResponseDTO>>
+	{
+		private readonly IIdentityService _identityService;
+
+		public GetRoleQueryHandler(IIdentityService identityService)
+		{
+			_identityService = identityService;
+		}
+		public async Task<IList<RoleResponseDTO>> Handle(GetRoleQuery request, CancellationToken cancellationToken)
+		{
+			var roles = await _identityService.GetRolesAsync();
+			return roles.Select(role => new RoleResponseDTO() { Id = role.id, RoleName = role.roleName }).ToList();
+		}
+	}
+}
